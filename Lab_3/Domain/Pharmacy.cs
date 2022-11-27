8182f1738bf24a8ea4175a9ad37c40d78dbc74ea
thinteen <80061280+thinteen@users.noreply.github.com>
2022-11-27 22:39:25 +0300

    Add files via upload

---
+++ Lab_3/Domain/Pharmacy.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3.Domain
+{
+    public class Pharmacy
+    {
+        public Pharmacy(string name, string address, List<Medicine> medicines)
+        {
+            Name = name;
+            Address = address;
+            Medicines = medicines;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Medicine> Medicines { get; set; }
+
+        public void AddMedicine(Medicine medicine)
+        {
+            if (Medicines.FirstOrDefault(item => item.Name == medicine.Name) != null)
+            {
+                throw new ArgumentException();
+            }
+
+            Medicines.Add(medicine);
+        }
+
+        public void DeleteAllMedicines()
+        {
+            Medicines.Clear();
+        }
+
+        public Medicine GetMedicineByName(string name)
+        {
+            Medicine medicine = Medicines.FirstOrDefault(item => item.Name == name);
+
+            if (medicine == null)
+            {
+                throw new ArgumentException($"Student with name {name} was not found ");
+            }
+
+            return medicine;
+        }
+
+        public List<Medicine> GetMedicinesOrderedByPrice()
+        {
+            var orderedMedicines = Medicines.OrderByDescending(item => item.Price).ToList();
+
+            if (orderedMedicines.Count == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return orderedMedicines;
+        }
+
+        public void DeleteMedicineByName(string name)
+        {
+            Medicine medicine = Medicines.FirstOrDefault(item => item.Name == name);
+
+            if (medicine == null)
+            {
+                throw new ArgumentException($"Student with name {name} was not found ");
+            }
+
+            Medicines.Remove(medicine);
+        }
+    }
+}
---
+++ Lab_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Lab_3.Domain;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string pharmacyName = "Дежурный аптекарь";
+            const string pharmacyAddress = "г.Йошкар-Ола";
+            List<Medicine> medicines = new();
+
+            Pharmacy pharmacy = new(pharmacyName, pharmacyAddress, medicines);
+
+            const string medicineName = "Йод";
+            const string medicineName2 = "Спирт";
+            const decimal medicinePrice = 49.89m;
+            const decimal medicinePrice2 = 59.89m;
+
+            Medicine medicine = new(medicineName, medicinePrice);
+
+            //pharmacy.AddMedicine(medicine);
+            //pharmacy.DeleteMedicineByName(medicineName2);
+            //Assert.True(pharmacy.GetAll()[0].Name == expectedMedicines[0].Name);
+            Console.WriteLine(medicines.Count);
+        }
+    }
+}
